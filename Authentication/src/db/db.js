52bf8469add2f01e7b34e9813b4bc35dfab47118
Rahul18A09 <rahulbharda60@gmail.com>
2026-05-02 11:09:29 +0530

    authentication practice

--- Authentication/src/db/db.js
+++ Authentication/src/db/db.js
@@ -4,7 +4,7 @@ async function connectDB() {
     try{
 
         await mongoose.connect(process.env.MONGO_URL);
-        console.log(process.env.MONGO_URL);
+        // console.log(process.env.MONGO_URL);
         
         console.log("MongoDB Connected");
         
